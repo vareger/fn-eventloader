fbf35ae4cc43d175b0c5d9a747072cb274dce36e
MaximFischuk <mfischuk@vareger.com>
2019-03-13 10:10:10 +0200

    Removed unused code

--- src/main/java/ethereum/eventloader/EventLoader.java
+++ src/main/java/ethereum/eventloader/EventLoader.java
@@ -2,8 +2,6 @@ package ethereum.eventloader;
 
 import ethereum.eventloader.metrics.EventMetrics;
 import io.micrometer.core.annotation.Timed;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.atomic.DistributedAtomicLong;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
@@ -16,13 +14,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.web3j.protocol.core.methods.response.EthLog.LogResult;
-import org.web3j.protocol.core.methods.response.EthSyncing;
 
-import java.io.File;
 import java.math.BigInteger;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -32,7 +25,6 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class EventLoader {
     private static final Logger log = LoggerFactory.getLogger(EventLoader.class);
-    private static final Logger INFO_EMAIL = LoggerFactory.getLogger("ethereum.eventloader.INFO_EMAIL");
 
     private static final String ZNODE_PROCESSED_BLOCK = "/processed_block";
 
@@ -49,22 +41,8 @@ public class EventLoader {
     @Value("${eventloader.sleep_interval_ms:3000}")
     private long sleepIntervalMs;
 
-    /**
-     * Milliseconds between email reports
-     */
-    @Value("${eventloader.report_email_interval_ms:3600000}")
-    private long reportEmailIntervalMs;
-
-    @Value("${eventloader.eth.data_folder:/eth_data}")
-    private String dataFolder;
-
-    @Value("${eventloader.eth.log_folder:/home/ubuntu/logs}")
-    private String logFolder;
-
     @Value("#{web3jConfig.startBlock}")
     private BigInteger startBlock;
-
-    private Stats stats;
 
     @Autowired
     public EventLoader(BlockchainAdapter blockchain, MessageBrokerAdapter messageBroker, CuratorFramework curatorFramework, EventMetrics metrics) {
@@ -72,7 +50,6 @@ public class EventLoader {
         this.messageBroker = messageBroker;
         this.curatorFramework = curatorFramework;
         this.metrics = metrics;
-        stats = new Stats();
     }
 
     @Timed(longTask = true, value = "loading_time")
@@ -82,8 +59,7 @@ public class EventLoader {
         try {
             atLatestBlock = this.metrics.recordExecutionTime(this::eventLoadAttempt);
         } catch (Exception e) {
-            stats.errors++;
-            log.error("Event load failed, will reconnect and retry", e);
+            log.error("Event load failed, will retry", e);
             sleep(sleepIntervalMs);
         }
 
@@ -94,62 +70,11 @@ public class EventLoader {
         } catch (InterruptedException ignored) { }
     }
 
-    @Scheduled(fixedDelayString = "#{${eventloader.report_email_interval_ms:3600000}}", initialDelay = 3600000)
-    public void sendEmail() {
-        emailReport();
-        stats = new Stats();
-    }
-
-    private void emailReport() {
-        NumberFormat fmt = new DecimalFormat("#0.00");
-
-        StringBuilder b = new StringBuilder();
-        b.append("Disk usage: ").append(dataFolder).append("\n");
-        b.append(fmt.format(diskUsagePct(dataFolder))).append("%");
-
-        b.append("\n\nDisk usage: ").append(logFolder).append("\n");
-        b.append(fmt.format(diskUsagePct(logFolder))).append("%");
-
-        b.append("\n\nStats")
-                .append("\n  lag: ").append(stats.lag)
-                .append("\n  queue: ").append(stats.queueSize)
-                .append("\n  blocks-processed: ").append(stats.blocksProcessed)
-                .append("\n  events-published: ").append(stats.eventsPublished)
-                .append("\n  locks-obtained: ").append(stats.locks)
-                .append("\n  errors: ").append(stats.errors);
-
-        b.append("\n\nSyncing: ");
-
-        EthSyncing res = blockchain.syncing();
-        if (!res.isSyncing()) {
-            b.append("No");
-        } else {
-            b.append("Yes");
-            EthSyncing.Syncing sync = (EthSyncing.Syncing) res.getResult();
-            int current = Integer.parseInt(sync.getCurrentBlock().substring(2), 16);
-            b.append("\n  current: ").append(current);
-            int latest = Integer.parseInt(sync.getHighestBlock().substring(2), 16);
-            b.append("\n  latest:  ").append(latest);
-            b.append("\n  pending: ").append(latest - current);
-        }
-
-        b.append("\n\nGenerated at: ").append(new Date());
-
-        INFO_EMAIL.info(b.toString());
-    }
-
-    static double diskUsagePct(String path) {
-        File folder = new File(path);
-        if (!folder.exists() || !folder.isDirectory()) {
-            throw new RuntimeException("Folder expected: " + folder);
-        }
-        return 100 - ((double) folder.getFreeSpace() / folder.getTotalSpace()) * 100;
-    }
-
     private static void sleep(long interval) {
         try {
             Thread.sleep(interval);
         } catch (InterruptedException ex) {
+            log.error("Sleep error", ex);
         }
     }
 
@@ -185,7 +110,6 @@ public class EventLoader {
             Events events;
             if (latestBlock > lastProcessed) {
                 events = blockchain.eventsLog(lastProcessed, latestBlock);
-                stats.lag = 0;
             } else if (lastProcessed > latestBlock) {
                 long lag = lastProcessed - latestBlock;
                 if (lag > 50) {
@@ -194,23 +118,18 @@ public class EventLoader {
                     log.info("Node is on block {} while latest processed is {}. Lag: {}", latestBlock, lastProcessed, lag);
                 }
                 waitForNodeToCatchup(lag);
-                stats.lag = lag;
                 return true;
             } else {
                 log.info("At latest block: {}", latestBlock);
-                stats.lag = 0;
                 return true;
             }
-            stats.locks++;
 
             long blocks = events.getEndBlock() - lastProcessed;
-            stats.blocksProcessed += blocks > 0 ? blocks : 0;
             List<LogResult> logs = events.getLogs(lastProcessed);
             blockchain.loadBlocks(lastProcessed, events.getEndBlock()).sequential().subscribe(messageBroker::publishBlock).dispose();
             if (logs.isEmpty()) {
                 log.info("All events published in parallel");
             } else {
-                stats.eventsPublished += logs.size();
                 messageBroker.publish(logs);
             }
 
@@ -228,8 +147,6 @@ public class EventLoader {
             this.metrics.addProcessedEventsCount((long)logs.size());
             this.metrics.addProcessedBlocksCount(blocks > 0 ? blocks : 0);
             this.metrics.setLatestBlockNumber(latestBlock);
-            long queueSize = latestBlock - Math.max(lastProcessed, events.getEndBlock());
-            stats.queueSize = queueSize > 0 ? queueSize : 0;
         } catch (Exception e) {
             log.error("Loader error", e);
         }
@@ -243,24 +160,6 @@ public class EventLoader {
         } else if (gap > 100) {
             log.info("Sleeping for 30 sec...");
             sleep(30000);
-        }
-    }
-
-    public void setSleepIntervalMs(long sleepIntervalMs) {
-        this.sleepIntervalMs = sleepIntervalMs;
-    }
-
-    private static class Stats {
-        int eventsPublished;
-        int blocksProcessed;
-        long lag;
-        long queueSize;
-        int locks;
-        int errors;
-
-        @Override
-        public String toString() {
-            return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
         }
     }
 }
